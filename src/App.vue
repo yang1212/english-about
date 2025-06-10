dc4317725994bfe7f1b5c2d62b8a3eef2e81a7db
杨伏 <1204914685@qq.com>
2025-06-10 11:53:41 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default {
           name: '学习成长',
           isOpen: false,
           categories: [
-            { id: 'myVedio', name: '听力视频/文档整理', description: '小红书账号-不和乔治完的佩奇' },
+            { id: 'myVedio', name: '听力视频/文档整理', description: '小红书账号-不和乔治玩的佩奇' },
             { id: 'myVedio2', name: '文档整理', description: '小红书账号-Sahra每日学英语' },
             { id: 'rendering', name: '造句翻译', description: '' },
             // { id: 'ai', name: '问题截图', description: '' }
